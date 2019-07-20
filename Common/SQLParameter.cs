fb694d512f343aa67fc0cfa7aebff456fd9051c9
jimny.chu <jimny.chu@easyuse.com.tw>
2019-07-20 16:53:56 +0800

    PROJ-0001 傳遞SQL參數時所需的類別庫牽入。

---
+++ Common/SQLParameter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class SQLParameter
+    {
+        public class ListSQLParameter
+        {
+            public string name = "";
+            public int size = 0;
+            public string value = "";
+            public Enum_Type_code type = new Enum_Type_code();
+
+            public enum Enum_Type_code : int
+            {
+                Int = 0,
+                NVarChar = 1,
+                DateTime = 2,
+            }
+
+        }
+
+        public class Enum_Type_Transfer
+        {
+            public readonly string[] Enum_Type = { "Int", "NVarChar", "DateTime" };
+        }
+
+        /// <summary>
+        /// 建立 input param. 也就是 stored procedure 所要用到的參數
+        /// </summary>
+        /// <param name="ParamName">Name of param.</param>
+        /// <param name="DbType">Param type.</param>
+        /// <param name="Size">Param size.</param>
+        /// <param name="Value">Param value.</param>
+        /// <returns>New parameter.</returns>
+        public static SqlParameter MakeInParam(string ParamName, SqlDbType DbType, int Size, object Value)
+        {
+            return MakeParam(ParamName, DbType, Size, ParameterDirection.Input, Value);
+        }
+
+        /// <summary>
+        /// 建立 stored procedure 參數.
+        /// </summary>
+        /// <param name="ParamName">Name of param.</param>
+        /// <param name="DbType">Param type.</param>
+        /// <param name="Size">Param size.</param>
+        /// <param name="Direction">Parm direction.</param>
+        /// <param name="Value">Param value.</param>
+        /// <returns>New parameter.</returns>
+        public static SqlParameter MakeParam(string ParamName, SqlDbType DbType, Int32 Size, ParameterDirection Direction, object Value)
+        {
+            SqlParameter param;
+
+            if (Size > 0)
+                param = new SqlParameter(ParamName, DbType, Size);
+            else
+                param = new SqlParameter(ParamName, DbType);
+
+            param.Direction = Direction;
+            if (!(Direction == ParameterDirection.Output && Value == null))
+                param.Value = Value;
+
+            return param;
+        }
+    }
+}
