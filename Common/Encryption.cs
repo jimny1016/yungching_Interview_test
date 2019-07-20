7558b5f24cff4103b63f8e376ac6e57b707806d4
jimny.chu <jimny.chu@easyuse.com.tw>
2019-07-20 16:51:59 +0800

    PROJ-0001 Common建置，加解密類別庫牽入。

---
+++ Common/Encryption.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Encryption
+    {
+        /// <summary>
+        /// base64加密
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        static public string EncryptBase64(string input)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+            return Convert.ToBase64String(inputBytes);
+        }
+
+        /// <summary>
+        /// base64解密
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        static public string DecryptBase64(string input)
+        {
+            byte[] inputBytes = Convert.FromBase64String(input);
+
+            return Encoding.UTF8.GetString(inputBytes);
+        }
+    }
+}
