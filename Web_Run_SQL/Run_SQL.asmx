c95b085f951f992abb27f519514f26386963405f
jimny.chu <jimny.chu@easyuse.com.tw>
2019-07-20 13:44:43 +0800

    PROJ-0001 初始API專案建置。

---
+++ Web_Run_SQL/Run_SQL.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Run_SQL.asmx.cs" Class="Web_Run_SQL.Run_SQL" %>
---
+++ Web_Run_SQL/Run_SQL.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace Web_Run_SQL
+{
+    /// <summary>
+    ///Run_SQL 的摘要描述
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // 若要允許使用 ASP.NET AJAX 從指令碼呼叫此 Web 服務，請取消註解下列一行。
+    // [System.Web.Script.Services.ScriptService]
+    public class Run_SQL : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
